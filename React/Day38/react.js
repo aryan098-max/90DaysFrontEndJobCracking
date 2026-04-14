3908b19429e4105e517df11a41cd7b25b9c21578
Aryan Gupta <aryangupta4279@gmail.com>
2026-04-14 23:26:55 +0530

    Day 38

---
+++ React/Day38/react.js
@@ -0,0 +1,12 @@
+/*
+    1. React: 
+    
+    React is a JavaScript library for building user interfaces, especially single-page applications. 
+    It uses a component-based architecture and a virtual DOM to efficiently update and render UI when 
+    the state changes.
+
+    2. What is React Component
+
+    
+
+*/
