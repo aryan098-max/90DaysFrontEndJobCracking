195264353c58e06fd4a581e971d0f387ca840e28
Aryan Gupta <aryangupta4279@gmail.com>
2026-04-17 23:19:01 +0530

    Day 41

--- React/Day40/useEffect.js
+++ React/Day40/useEffect.js
@@ -1,8 +1,14 @@
 /*
     1. useEffect():
 
-    Note: useEffect() schedules an async callback operation, which runs only after the page has rendered once.
-    It takes two arguments a callback function and array dependency []. 
+    It is a react hook which runs after a component re-renders. It takes a callback function and
+    a dependency array, which controls when the effect runs
+
+    Three scenarios
+
+    1. useEffect(()=>{}, []) - run once (on mount)
+    2. useEffect(()=>{},[state variable]) - runs when state change
+    3. useEffect(()=>{}, ) - no dependency array runs on every render
 
     This hook is used for 
     1. api calls, 
